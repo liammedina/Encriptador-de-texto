6b4ed47af07c2169cf9713eac9e5bdb6b3b998c9
Lia Medina <liamedina@hotmail.com>
2024-08-26 15:42:42 +0000

    Se agrega funcionalidad

--- app.js
+++ app.js
@@ -5,6 +5,11 @@ let imgUff = document.querySelector('.imgRobot');
 let respuesta = document.querySelector('.respuesta');
 let contenido = document.querySelector('.conteinerColumDer');
 let btnCopiar = document.querySelector('.btnCopiar');
+let desencriptar = document.querySelector('.btnDesencriptar');
+let imgDialogo = document.querySelector('imgRobotDialogo');
+
+
+
 
 btnEncriptar.addEventListener('click', e=> {
     e.preventDefault();
@@ -44,19 +49,63 @@ btnEncriptar.addEventListener('click', e=> {
         texto = texto.replace(/a/mg, 'ai');
         texto = texto.replace(/o/mg, 'ober');
         texto = texto.replace(/u/mg, 'ufat');
-
-        texto.innerHTML = texto;
+        
+      
+        respuesta.innerHTML = texto;
         btnCopiar.style.visibility = 'inherit';
+        //imgDialogo.style.visibility = 'inherit'
         contenido.remove();
-        
-        respuesta.style.visibility = 'inherit';
-        
-        
 
     } 
 })
     
+btnDesencriptar.addEventListener('click', e=> {
+    e.preventDefault();
+    let texto = textoEncriptar.value;
+    let textNorm = texto.normalize('NFD').replace(/[$\.¿\?~!\¡@#%^&*()_|}\{[\]>\<:"`;,\u0300-\u036f']/g, " " );
+    
+    if(texto == ''){
+        aviso.style.color = '#ffb000';
+        aviso.textContent = 'Debe incluir un texto para encriptar';
+        imgUff.src = './multimedia/robotUff.png';
 
+        setTimeout(()=>{
+            aviso.removeAttribute('style');
+        },1500);
+    }
+    else if(texto !== textNorm){
+        aviso.style.color = '#ffb000';
+        aviso.textContent = 'El texto no debe tener acentos ni caracteres especiales';
+        imgUff.src = './multimedia/robotUff.png';
+
+        setTimeout(()=>{
+            aviso.removeAttribute('style');
+        },1500);
+    }
+    else if(texto !== texto.toLowerCase()){
+        aviso.style.color = '#ffb000';
+        aviso.textContent = 'El texto debe ser todo en minúscula';
+        imgUff.src = './multimedia/robotUff.png';
+
+        setTimeout(()=>{
+            aviso.removeAttribute('style');
+        },1500);
+    }
+    else{
+        texto = texto.replace(/enter/mg, 'e');
+        texto = texto.replace(/imer/mg, 'i');
+        texto = texto.replace(/ai/mg, 'a');
+        texto = texto.replace(/ober/mg, 'o');
+        texto = texto.replace(/ufat/mg, 'u');
+        
+      
+        respuesta.innerHTML = texto;
+        btnCopiar.style.visibility = 'inherit';
+
+        contenido.remove();
+
+    } 
+})
 
 
 
